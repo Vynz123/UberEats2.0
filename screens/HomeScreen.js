220b77c1770a452d94eb95d3c138d562d35a22c6
Vynz123 <shubhayc14@gmail.com>
2023-08-27 10:18:10 -0700

    Init

---
+++ screens/HomeScreen.js
@@ -0,0 +1,18 @@
+import { View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import Header from '../components/home/Header'
+import Hero from '../components/home/Hero'
+import Calender from '../components/home/Calender'
+import TaskShower from '../components/home/TaskShower'
+import BottomTabs from '../components/home/BottomTabs'
+
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView>
+      
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
